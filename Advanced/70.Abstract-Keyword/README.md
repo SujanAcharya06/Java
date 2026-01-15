7521af9ddc2670d7053622a0a7d13a9182f94b20
SujanAcharya06 <sujanjacharya123@gmail.com>
2026-01-15 14:21:30 +0530

    docs(abstract-keyword): elaborate on abstract class constructors
    README.md: Added detailed explanation regarding abstract class
    constructors, clarifying they can exist and be invoked by child
    classes, and illustrating reference/object type compatibility.
    
    test.java: Applied minor formatting adjustments for consistency and readability, including indentation and brace placement.

--- Advanced/70.Abstract-Keyword/README.md
+++ Advanced/70.Abstract-Keyword/README.md
@@ -4,6 +4,10 @@
 - An abstract method must be declared inside an abstract class
 - If we are exteding an abstract class we must define/override the abstract method
 - We can't create object of abstract class
+	- but we can have a `constructor` of abstract class
+	- this is because the child class can instantiate it
+		- we can have a reference type of abstract class and the object type of child class
+			- `A obj = new B()`
 - We can create a reference of abstract of class
 - It is not compulsory to for an abstract class to have abstract method
 - If a car cannot implement all the methods then that class can be made abstract class as well
--- Advanced/70.Abstract-Keyword/test.java
+++ Advanced/70.Abstract-Keyword/test.java
@@ -6,33 +6,34 @@
 // It is not compulsory to for an abstract class to have abstract method
 // If a car cannot implement all the methods then that class can be made abstract class as well
 
+abstract class Car {
+	public abstract void drive(); // abstract method
 
-abstract class Car{
-    public abstract void drive(); // abstract method
-    public abstract void indicator();
-    public void playMusic(){
-        System.out.println("Play music");
-    }
+	public abstract void indicator();
+
+	public void playMusic() {
+		System.out.println("Play music");
+	}
 }
 
-abstract class WagonR extends Car{
-    public void drive(){
-        System.out.println("Driving...");
-    }
+abstract class WagonR extends Car {
+	public void drive() {
+		System.out.println("Driving...");
+	}
 }
 
-class UpdatedWagonR extends WagonR{ // Concrete class
-    public void indicator(){
-        System.out.println("Indicator features");
-    }
+class UpdatedWagonR extends WagonR { // Concrete class
+	public void indicator() {
+		System.out.println("Indicator features");
+	}
 
 }
 
 public class test {
-    public static void main(String[] args) {
-        // Car car = new WagonR(); // can create reference
-        Car car = new UpdatedWagonR(); // can create reference
-        car.drive();
-        car.playMusic();
-    }
+	public static void main(String[] args) {
+		// Car car = new WagonR(); // can create reference
+		Car car = new UpdatedWagonR(); // can create reference
+		car.drive();
+		car.playMusic();
+	}
 }
