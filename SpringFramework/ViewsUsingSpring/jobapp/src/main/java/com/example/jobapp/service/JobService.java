b38289462064cc6edc31648f9817c8bb8b78b66c
SujanAcharya06 <sujanjacharya123@gmail.com>
2026-03-09 23:12:01 +0530

    impl(JobService.java)   - Add `Service` class, with methods `addJob` and `getAllJobs`   - @Autowired repoisotry class `JobRepo`

---
+++ SpringFramework/ViewsUsingSpring/jobapp/src/main/java/com/example/jobapp/service/JobService.java
@@ -0,0 +1,27 @@
+
+package com.example.jobapp.service;
+
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.example.jobapp.model.JobPost;
+import com.example.jobapp.repo.JobRepo;
+
+@Service
+public class JobService {
+
+	@Autowired
+	private JobRepo jobRepo;
+
+	public void addJob(JobPost job) {
+		jobRepo.addJob(job);
+
+	}
+
+	public List<JobPost> getAllJobs() {
+		return jobRepo.getAllJobs();
+	}
+
+}
