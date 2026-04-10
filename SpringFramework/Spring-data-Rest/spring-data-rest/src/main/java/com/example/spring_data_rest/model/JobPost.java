47162f9664aa8c0d9b38cc320a0bb564724e103a
SujanAcharya06 <sujanjacharya123@gmail.com>
2026-04-10 09:50:00 +0530

    feat(spring-data-rest): add job post entity and repository
    SpringFramework/Spring-data-Rest/spring-data-rest/src/main/java/com/example/spring_data_rest/SpringDataRestApplication.java:
    Initializes the main Spring Boot application.
    SpringFramework/Spring-data-Rest/spring-data-rest/src/main/java/com/example/spring_data_rest/model/JobPost.java:
    Defines the  JPA entity with fields for job details, including
    annotations for persistence and Lombok for boilerplate code.
    SpringFramework/Spring-data-Rest/spring-data-rest/src/main/java/com/example/spring_data_rest/repo/JobRepo.java:
    Creates the  interface extending , enabling
    automatic REST endpoint exposure for  entities.

---
+++ SpringFramework/Spring-data-Rest/spring-data-rest/src/main/java/com/example/spring_data_rest/model/JobPost.java
@@ -0,0 +1,25 @@
+package com.example.spring_data_rest.model;
+
+import java.util.List;
+
+import org.springframework.stereotype.Component;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.Id;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data // for getter setters
+@AllArgsConstructor // all fields constructor
+@NoArgsConstructor // empty constructor
+@Component
+@Entity
+public class JobPost {
+	@Id
+	private int postId;
+	private String postProfile;
+	private String postDesc;
+	private int reqExperience;
+	private List<String> postTechStack;
+}
---
+++ SpringFramework/Spring-data-Rest/spring-data-rest/src/main/java/com/example/spring_data_rest/repo/JobRepo.java
@@ -0,0 +1,11 @@
+package com.example.spring_data_rest.repo;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import com.example.spring_data_rest.model.JobPost;
+
+@Repository
+public interface JobRepo extends JpaRepository<JobPost, Integer> {
+
+}
